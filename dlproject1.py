e0045087b67c07d2d7c486e8a6e8eb19aa77398c
Cole Felix <89165165+VolsFelix@users.noreply.github.com>
2022-02-27 17:20:47 -0500

    hotcoded y

--- dlproject1.py
+++ dlproject1.py
@@ -30,11 +30,6 @@ i = 0
 
 
 
-Y[0]
-
-
-
-
 for d in parse('meta_Clothing_Shoes_and_Jewelry.json.gz'):
     i += 1
     X = (d['title'])
@@ -65,6 +60,8 @@ unique_cats=[]
 for i in categories:
     for j in i:
         cat_list.append(j)
+
+### Function to find the max length of the categories for padding
 def lengths(x):
     if isinstance(x,list):
         yield len(x)
@@ -81,7 +78,7 @@ for i in range(len(cat_list)):
 for i in range(len(categories)):
     for j in range(len(categories[i])):
         categories[i][j]=categories[i][j].ljust(max(lengths(cat_list)), '0')
-          
+
 ### padded unique cats       
 for b in cat_list:
     if b not in unique_cats:
@@ -107,32 +104,26 @@ titles[99]
 vectorizer = CountVectorizer(min_df=0, lowercase=False,ngram_range=(max(lengths(cat_list)),max(lengths(cat_list))), analyzer='char')
 vectorizer.fit(unique_cats)
 vectorizer.vocabulary_
-Y=vectorizer.transform(categories)
+Y=vectorizer.transform(char_cat)
 ###
+#### hot coding Y
+
+##make each entry of categories into a string for Y
+char_cat=[]
+for i in range(len(categories)):
+    char_cat.append(str(categories[i]))
 
 Hotcoded_Y = pd.DataFrame(Y.toarray(),columns=vectorizer.get_feature_names())
 Hotcoded_Y.head()
 Hotcoded_Y.iloc[0].sum()
+len(categories[0])
+len(Hotcoded_Y)
 
-(categories[0])
-titles[0]
-titles
-str(categories)
-len(cat_list)
+
+
 ### Office hours
 ## feed in X variables 1 by 1. single row is 1X50000. Make list of unique cats, 10000. matrix is nx10000. set value to 1 where corresponding cat
 #50000 x 100000 y every x is 1 except for where the column corresponds to the word in the title. 
 # for category not looking at words, looking at entire category
-len(unique_cats)
-len(categories[0][1].ljust(22,' '))
-len(categories)
-len(cat_list)
-html = categories.decode('U92')
-type(titles)
-type(categories)
-## Find max lenth of categories
-    
-categories[0]
-cat_list[0]
-type(categories)
-type(titles)
+
+

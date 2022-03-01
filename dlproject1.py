fe8177b419f9cf27af30a24e76b164cdf8bdec49
Cole Felix <89165165+VolsFelix@users.noreply.github.com>
2022-03-01 12:37:48 -0500

    kdjf

--- dlproject1.py
+++ dlproject1.py
@@ -69,6 +69,11 @@ def lengths(x):
             yield from lengths(y)
 max(lengths(cat_list))
 #####
+##### counting and plotting the most common categories
+from collections import Counter
+C=(Counter(cat_list))
+
+
 
 #Padding Cat list 
 for i in range(len(cat_list)):
@@ -100,6 +105,12 @@ df.head()
 Hotcoded_X.iloc[99].sum()
 titles[99]
 
+
+##make each entry of categories into a string for Y
+char_cat=[]
+for i in range(len(categories)):
+    char_cat.append(str(categories[i]))
+
  ########### category vecorization
 vectorizer = CountVectorizer(min_df=0, lowercase=False,ngram_range=(max(lengths(cat_list)),max(lengths(cat_list))), analyzer='char')
 vectorizer.fit(unique_cats)
@@ -108,22 +119,43 @@ Y=vectorizer.transform(char_cat)
 ###
 #### hot coding Y
 
-##make each entry of categories into a string for Y
-char_cat=[]
-for i in range(len(categories)):
-    char_cat.append(str(categories[i]))
+
 
 Hotcoded_Y = pd.DataFrame(Y.toarray(),columns=vectorizer.get_feature_names())
 Hotcoded_Y.head()
 Hotcoded_Y.iloc[0].sum()
 len(categories[0])
 len(Hotcoded_Y)
-
-
+categories[0]
 
 ### Office hours
 ## feed in X variables 1 by 1. single row is 1X50000. Make list of unique cats, 10000. matrix is nx10000. set value to 1 where corresponding cat
 #50000 x 100000 y every x is 1 except for where the column corresponds to the word in the title. 
 # for category not looking at words, looking at entire category
 
+Hotcoded_X.head()
+Hotcoded_Y.head()
 
+indices = np.array(range(len(unique_cats)), dtype = np.int64)
+indices
+lookuptable = np.column_stack([unique_cats,indices])
+
+
+inputs = tf.keras.layers.Input(shape=(Hotcoded_X.shape[1],), name='input') #Note: shape is a tuple and does not includes records. For a two dimensional input dataset, use (Nbrvariables,). We would use the position after the comma, if it would be a 3-dimensional tensor (e.g., images). Note that (something,) does not create a second dimension. It is just Python's way of generating a tuple (which is required by the Input layer).
+hidden1 = tf.keras.layers.Dense(units=555, activation="relu", name = 'hidden1')(inputs)
+hidden2 = tf.keras.layers.Dense(units=555, activation="relu", name= 'hidden2')(hidden1)
+outputs = tf.keras.layers.Dense(units=356, activation = "sigmoid", name= 'output')(hidden2)
+
+model = tf.keras.Model(inputs = inputs, outputs = outputs)
+model.compile(loss = 'binary_crossentropy', optimizer = tf.keras.optimizers.SGD(learning_rate = 0.001))
+model.fit(x=Hotcoded_X,y=Hotcoded_Y, batch_size=1, epochs=50)
+yhat = model.predict(x=Hotcoded_X)
+model.evaluate(x=Hotcoded_X,y=Hotcoded_Y)
+
+yhat[0].sum()
+sorted(range(len(yhat[50])), key=lambda i: yhat[0][i])[-5:]
+
+categories[50]
+titles[50]
+cat_list[148]
+lookuptable[148]
